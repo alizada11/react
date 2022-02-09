b6666a04ab4d00e9ad0cfcfd9d0c60baa9ee0d0d
Rana Zare <eng.tender7@gmail.com>
2022-02-09 14:15:51 +0330

    Added REST Api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Tweet from './tweet';
 
-function App() {
+function App(){
+  
+  const [isRed, setRed]  = useState(false);
+  const [count, setCount] = useState(0);
+  const increment = () =>{
+    setCount(count +1);
+    setRed(!isRed);
+  };
+
+  const [users, setUsers] = useState([
+    {name: "Ed", message:"I am Ed", likes:"3.0 k"},
+    {name: "John", message:"A tweet From John", likes:"3.2 k"},
+    {name: "Mohammad", message:"This is Mohammad", likes:"3.1 k"}
+  ]);
+
+  const decrement = () =>{
+    setCount(count - 1);
+  };
+
+  const reset = () =>{
+    setCount(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {users.map(user=> (
+          <Tweet name={user.name} message={user.message} />
+      ))}
+      <h1 className={isRed ? "red" : ""}>Change my color</h1>
+      <button onClick = {increment}>Increament</button>
+      <h1>{count}</h1>
+      <button onClick= {decrement}>decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
+    
   );
 }
 
